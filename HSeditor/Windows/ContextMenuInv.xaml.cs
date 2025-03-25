96e0b1c7786767cbf0a20525b7f974bab2b7f6b0
Logi <129036868+LogiOnKick@users.noreply.github.com>
2025-03-25 18:57:48 -0400

    v.0.0.3 Random seeds for whole tab with one click

--- HSeditor/Windows/ContextMenuInv.xaml.cs
+++ HSeditor/Windows/ContextMenuInv.xaml.cs
@@ -90,6 +90,71 @@ namespace HSeditor.Windows
             }
         }
 
+        private void ContextMenu_NewSeedAll_Click(object sender, RoutedEventArgs e)
+        {
+            this.IsOpen = false;
+            
+            switch (bindStruct.Origin)
+            {
+                case DragHandler.Origin.Equip:
+                    // Randomize seeds for all equipped items
+                    foreach (var slot in MainWindow.INSTANCE.SaveFileHandler.SelectedFile.Inventory.Equipment.GetEquipment())
+                    {
+                        if (slot.Item != null)
+                        {
+                            int randomSeed = MainWindow.INSTANCE.random.Next(1, 100000000);
+                            
+                            if (!slot.Item.SaveItem.ContainsKey("seed"))
+                                slot.Item.SaveItem.Add("seed", randomSeed);
+                            else
+                                slot.Item.SaveItem["seed"] = randomSeed;
+
+                            slot.Item.RollID = randomSeed;
+                        }
+                    }
+                    MainWindow.INSTANCE.UpdateEquippedItems();
+                    break;
+                    
+                case DragHandler.Origin.Inventory:
+                    // Randomize seeds for all items in the current inventory tab
+                    foreach (var item in MainWindow.INSTANCE.SaveFileHandler.SelectedFile.Inventory.InventoryItems)
+                    {
+                        if (item.Inv == MainWindow.INSTANCE.SelectedInv)
+                        {
+                            int randomSeed = MainWindow.INSTANCE.random.Next(1, 100000000);
+                            
+                            if (!item.SaveItem.ContainsKey("seed"))
+                                item.SaveItem.Add("seed", randomSeed);
+                            else
+                                item.SaveItem["seed"] = randomSeed;
+
+                            item.RollID = randomSeed;
+                        }
+                    }
+                    MainWindow.INSTANCE.UpdateInventory();
+                    break;
+                    
+                case DragHandler.Origin.Stash:
+                    // Randomize seeds for all items in the current stash tab
+                    foreach (var item in MainWindow.INSTANCE.SaveFileHandler.Shop.Stash)
+                    {
+                        if (item.Inv == MainWindow.INSTANCE.SelectedStash)
+                        {
+                            int randomSeed = MainWindow.INSTANCE.random.Next(1, 100000000);
+                            
+                            if (!item.SaveItem.ContainsKey("seed"))
+                                item.SaveItem.Add("seed", randomSeed);
+                            else
+                                item.SaveItem["seed"] = randomSeed;
+
+                            item.RollID = randomSeed;
+                        }
+                    }
+                    MainWindow.INSTANCE.UpdateStash();
+                    break;
+            }
+        }
+
         private void Open(object sender, RoutedEventArgs e)
         {
             this.UpdateLayout();
